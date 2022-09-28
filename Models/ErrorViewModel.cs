58ab407d8cc089e9c97e463eb24fed95f6b711d4
Android Studio <devandroid@ort>
2022-09-28 08:54:09 -0300

    Add Project Files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ejemplo_Github.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
